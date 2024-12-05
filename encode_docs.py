2399b9b8c0b1d703575ccb72942f91fbd1d4e4ed
Pooja Thakur <113713900+Pooja12-cpu@users.noreply.github.com>
2024-12-05 00:20:58 -0500

    Encode docs using any sentence transformer model

---
+++ encode_docs.py
@@ -0,0 +1,44 @@
+from sentence_transformers import SentenceTransformer, util
+import numpy as np
+import jsonlines
+
+#Enter the name of the bi-encoder model to use
+model_name = ""
+
+#Enter the name and location of file where Beauty_and_Fashon data is stored
+data_file = ""
+
+#Enter filename to save encoded data
+filename_encoded_data = ""
+#Enter filename to save row_to_doc list
+filename_ordering_data = ""
+
+biencoder_model = SentenceTransformer(model_name)
+
+#Read file data
+with jsonlines.open(data_file) as file:
+    data = list(file.iter())
+
+print('Processing data...')
+row_to_doc = []
+#Iterates through all product data and creates embeddings for a combined string of title and description
+for i in range(len(data)):
+    if i==0:
+        combined_input = data[i]['title'] + " " + data[i]['description']
+        embedding = biencoder_model.encode(combined_input)
+        embedded_mat = np.zeros((len(data),len(embedding)))
+        embedded_mat[i] = embedding
+        row_to_doc.append(data[i]['docid'])
+    else:
+        combined_input = data[i]['title'] + " " + data[i]['description']
+        embedding = biencoder_model.encode(combined_input)
+        embedded_mat[i] = embedding
+        row_to_doc.append(data[i]['docid'])
+
+np.save(filename_encoded_data, embedded_mat)
+np.save(filename_ordering_data, np.array(row_to_doc))
+print('Data saved.')
+
+
+
+
